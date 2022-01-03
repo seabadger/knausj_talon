eeb4459a19576682822e47f5c38171b43072dca0
Vadim Grinshpun <vg23@cornell.edu>
2022-01-03 13:41:10 -0500

    tweaks

--- code/keys.py
+++ code/keys.py
@@ -241,6 +241,7 @@ alternate_keys = {
     #'junk': 'backspace',
     "page up": "pageup",
     "page down": "pagedown",
+    "blank": "space",
 }
 # mac apparently doesn't have the menu key.
 if app.platform in ("windows", "linux"):
