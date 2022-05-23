2a1e6805d514aba83ca8fadf38a5d46d17786f8f
Vadim Grinshpun <vg23@cornell.edu>
2022-05-23 18:06:20 -0400

    update my merge script

--- merge.sh
+++ merge.sh
@@ -4,9 +4,12 @@ barf() {
     exit 1
 }
 
+STASHED=no
 if [ "$(git status --porcelain --untracked-files=no | wc -l| tr -d ' ')" != 0 ]
 then
-  barf "have local modifications, stash them or something"
+  echo "We have local modifications, stashing them!"
+  git stash || barf "failed to stash local modifications"
+  STASHED=yes
 fi
 
 git checkout knausj_master || barf "failed to checkout knausj_master"
@@ -16,3 +19,13 @@ git merge knausj_master
 git checkout vadim_personal  || barf "failed to checkout vadim_personal"
 git merge master
 git status
+
+if [ "$STASHED" == "yes" ];
+then
+    echo "stashed:"
+    git stash list || barf "git stash list failed"
+    echo "unstashing local modifications!"
+    git stash pop || barf "git stash pop failed"
+    echo "unstashed successfully"
+    git status
+fi
